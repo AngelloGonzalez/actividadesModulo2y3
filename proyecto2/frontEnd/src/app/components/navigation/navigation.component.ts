1e03e83a08b64f0dc7c140aaf9341baaad839808
Angelo Gonzalez <angellog9714@gmail.com>
2024-07-18 19:22:58 -0500

    formulario registro

--- proyecto2/frontEnd/src/app/components/navigation/navigation.component.ts
+++ proyecto2/frontEnd/src/app/components/navigation/navigation.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { LoginComponent } from '../login/login.component';
+import { RegistreComponent } from '../registre/registre.component';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-navigation',
   standalone: true,
-  imports: [],
+  imports: [LoginComponent, RegistreComponent, CommonModule],
   templateUrl: './navigation.component.html',
   styleUrl: './navigation.component.css'
 })
 export class NavigationComponent {
+
+  isVisibleLogin: boolean = false;
+  isVisibleRegister: boolean = false;
+
+  toggleLogin(){
+    this.isVisibleLogin = !this.isVisibleLogin;
+    this.isVisibleRegister = false;
+  }
+
+  toggleRegister(){
+    this.isVisibleRegister = !this.isVisibleRegister;
+    this.isVisibleLogin = false;
+  }
 
 }
--- proyecto2/frontEnd/src/app/pages/home/home.component.ts
+++ proyecto2/frontEnd/src/app/pages/home/home.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-
+import { NavigationComponent } from "../../components/navigation/navigation.component";
 
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [NavigationComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
